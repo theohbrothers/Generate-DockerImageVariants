0a84a62b459c0eefd0903bfb9fa9e6a485d38bca
Leo <leonardjonathanoh@live.com>
2018-10-20 14:32:15 +0800

    fix project path resolution; regenerate examples

--- Modules/Generate-DockerImageVariants/Generate-DockerImageVariants.psm1
+++ Modules/Generate-DockerImageVariants/Generate-DockerImageVariants.psm1
@@ -64,117 +64,119 @@ function Generate-DockerImageVariants {
         [string]$ProjectPath
     )
 
-    $PROJECT_BASE_DIR = $ProjectPath
-    $GENERATE_BASE_DIR = Join-Path $ProjectPath 'generate'
-    $GENERATE_TEMPLATES_DIR = Join-Path $GENERATE_BASE_DIR "templates"
-    $GENERATE_DEFINITIONS_DIR = Join-Path $GENERATE_BASE_DIR "definitions"
+    try {
+        $PROJECT_BASE_DIR = Resolve-Path $ProjectPath | Select-Object -ExpandProperty Path
+        $GENERATE_BASE_DIR = Join-Path $PROJECT_BASE_DIR 'generate'
+        $GENERATE_TEMPLATES_DIR = Join-Path $GENERATE_BASE_DIR "templates"
+        $GENERATE_DEFINITIONS_DIR = Join-Path $GENERATE_BASE_DIR "definitions"
 
-    $ErrorActionPreference = 'Stop'
+        $ErrorActionPreference = 'Stop'
 
-    Push-Location $GENERATE_BASE_DIR
+        Push-Location $GENERATE_BASE_DIR
 
-    # Get variants' definition
-    $VARIANTS = . ( Join-Path $GENERATE_DEFINITIONS_DIR "VARIANTS.ps1" )
+        # Get variants' definition
+        $VARIANTS = . ( Join-Path $GENERATE_DEFINITIONS_DIR "VARIANTS.ps1" )
 
-    # Get files' definition
-    $FILES = . ( Join-Path $GENERATE_DEFINITIONS_DIR "FILES.ps1" )
+        # Get files' definition
+        $FILES = . ( Join-Path $GENERATE_DEFINITIONS_DIR "FILES.ps1" )
 
-    # Intelligently add properties
-    $VARIANTS | % {
-        $VARIANT = $_
-        $VARIANTS_SHARED.GetEnumerator() | % {
-            $VARIANT[$_.Name] =  $_.Value
-        }
-        $VARIANT['tag_without_distro'] = if ( $VARIANT['distro'] ) {
-                                            # The variant's build directory name, stripped of the distro name if present
-                                            $variant_tag_regex = [regex]::Escape( $VARIANT['distro'] )
-                                            if ( $VARIANT['tag'] -match "^(.*)$variant_tag_regex(.*)$" ) {
-                                                "$( $matches[1] )-$( $matches[2] )".Trim('-')
-                                            }else {
-                                                $VARIANT['tag']
-                                            }
-                                        }else {
-                                                $VARIANT['tag']
-                                        }
-        $VARIANT['build_dir_rel'] = if ( $VARIANT['distro'] ) {
-                                    "variants/$( $VARIANT['distro'] )/$( $VARIANT['tag_without_distro'] )"
-                                }else {
-                                    "variants/$($VARIANT['tag'])"
-                                }
-        $VARIANT['build_dir'] = Join-Path "$PROJECT_BASE_DIR" $VARIANT['build_dir_rel']
-    }
-
-    # Generate each Docker image variant's build context files
-    $VARIANTS | % {
-        $VARIANT = $_
-
-        "Generating variant of name $( $VARIANT['tag'] ), variant dir: $( $VARIANT['build_dir'] )" | Write-Host -ForegroundColor Green
-        "Generating variant of name $( $VARIANT['tag'] ), variant dir: $( $VARIANT['build_dir'] )" | Write-Host -ForegroundColor Green
-        if ( ! (Test-Path $VARIANT['build_dir']) ) {
-            New-Item -Path $VARIANT['build_dir'] -ItemType Directory -Force > $null
-        }
-
-        # Generate Docker build context files
-        if ( $VARIANT['buildContextFiles'] ) {
-            # Templates
-            if ( $VARIANT['buildContextFiles']['templates'] -and $VARIANT['buildContextFiles']['templates'] -is [hashtable] ) {
-                $VARIANT['buildContextFiles']['templates'].GetEnumerator() | % {
-                    $templateFile = $_.Key
-                    $templateFileConfig = $_.Value
-                    $templateObject = @{
-                        TemplateFile = $templateFile
-                        TemplateDirectory = if ( $templateFileConfig['common'] ) {
-                                                $GENERATE_TEMPLATES_DIR
-                                            }else {
-                                                if ( $VARIANT['distro'] ) {
-                                                    "$GENERATE_TEMPLATES_DIR/$templateFile/$( $VARIANT['distro'] )"
+        # Intelligently add properties
+        $VARIANTS | % {
+            $VARIANT = $_
+            $VARIANTS_SHARED.GetEnumerator() | % {
+                $VARIANT[$_.Name] =  $_.Value
+            }
+            $VARIANT['tag_without_distro'] = if ( $VARIANT['distro'] ) {
+                                                # The variant's build directory name, stripped of the distro name if present
+                                                $variant_tag_regex = [regex]::Escape( $VARIANT['distro'] )
+                                                if ( $VARIANT['tag'] -match "^(.*)$variant_tag_regex(.*)$" ) {
+                                                    "$( $matches[1] )-$( $matches[2] )".Trim('-')
                                                 }else {
-                                                    "$GENERATE_TEMPLATES_DIR/$templateFile/"
+                                                    $VARIANT['tag']
                                                 }
+                                            }else {
+                                                    $VARIANT['tag']
                                             }
-                        Header = if ( $templateFileConfig['includeHeader'] ) { $true } else { $false }
-                        # Dynamically determine the sub templates from the name of the variant. (E.g. 'foo-bar' will comprise of foo and bar variant sub templates for this template file)
-                        SubTemplates =  if ( ! $templateFileConfig['common'] ) {
-                                            $VARIANT['tag'] -split '-' | % { $_.Trim() } | ? { $_ } | ? { $_ -ne $VARIANT['distro'] }
-                                        }else { @() }
-                        Footer = if ( $templateFileConfig['includeFooter'] ) { $true } else { $false }
-                    }
-
-                    $generatedFile = "$( $VARIANT['build_dir'] )/$templateFile"
-                    $templateFileConfig['passes'] | % {
-                        $pass = $_
-                        $templateObject['TemplatePassVariables'] = if ( $pass['variables'] ) { $pass['variables'] } else { @() }
-                        $generatedFile = if ( $pass['generatedFileNameOverride'] ) { "$( $VARIANT['build_dir'] )/$( $pass['generatedFileNameOverride'] )" } else { $generatedFile }
-                        $generatedFileContent = Get-ContextFileContent @templateObject
-                        $generatedFileContent | Out-File $generatedFile -Encoding Utf8 -Force -NoNewline
-                    }
-                }
-            }
-
-            # Copies
-            if ( $VARIANT['buildContextFiles']['copies'] ) {
-                $VARIANT['buildContextFiles']['copies'] | % {
-                    $blob = $_.Trim()
-                    # Any blob starting with '/' means we will
-                    if ($blob -match '^\/') {
-                        $fullPathBlob = Join-Path $PROJECT_BASE_DIR $blob
-                    }else {
-                        $fullPathBlob = "$GENERATE_TEMPLATES_DIR/variants/$( $VARIANT['tag'] )/$blob"
-                    }
-                    Copy-Item -Path $fullPathBlob -Destination $VARIANT['build_dir'] -Force -Recurse
-                }
-            }
+            $VARIANT['build_dir_rel'] = if ( $VARIANT['distro'] ) {
+                                        "variants/$( $VARIANT['distro'] )/$( $VARIANT['tag_without_distro'] )"
+                                    }else {
+                                        "variants/$($VARIANT['tag'])"
+                                    }
+            $VARIANT['build_dir'] = Join-Path "$PROJECT_BASE_DIR" $VARIANT['build_dir_rel']
         }
 
-    }
+        # Generate each Docker image variant's build context files
+        $VARIANTS | % {
+            $VARIANT = $_
 
-    # Generate other repo files
-    $FILES | % {
-        # Generate README.md
-        Get-ContentFromTemplate -Path (Join-Path $GENERATE_TEMPLATES_DIR "$_.ps1") | Out-File (Join-Path $PROJECT_BASE_DIR $_) -Encoding utf8 -NoNewline
-    }
+            "Generating variant of name $( $VARIANT['tag'] ), variant dir: $( $VARIANT['build_dir'] )" | Write-Host -ForegroundColor Green
+            "Generating variant of name $( $VARIANT['tag'] ), variant dir: $( $VARIANT['build_dir'] )" | Write-Host -ForegroundColor Green
+            if ( ! (Test-Path $VARIANT['build_dir']) ) {
+                New-Item -Path $VARIANT['build_dir'] -ItemType Directory -Force > $null
+            }
 
-    Pop-Location
+            # Generate Docker build context files
+            if ( $VARIANT['buildContextFiles'] ) {
+                # Templates
+                if ( $VARIANT['buildContextFiles']['templates'] -and $VARIANT['buildContextFiles']['templates'] -is [hashtable] ) {
+                    $VARIANT['buildContextFiles']['templates'].GetEnumerator() | % {
+                        $templateFile = $_.Key
+                        $templateFileConfig = $_.Value
+                        $templateObject = @{
+                            TemplateFile = $templateFile
+                            TemplateDirectory = if ( $templateFileConfig['common'] ) {
+                                                    $GENERATE_TEMPLATES_DIR
+                                                }else {
+                                                    if ( $VARIANT['distro'] ) {
+                                                        "$GENERATE_TEMPLATES_DIR/$templateFile/$( $VARIANT['distro'] )"
+                                                    }else {
+                                                        "$GENERATE_TEMPLATES_DIR/$templateFile/"
+                                                    }
+                                                }
+                            Header = if ( $templateFileConfig['includeHeader'] ) { $true } else { $false }
+                            # Dynamically determine the sub templates from the name of the variant. (E.g. 'foo-bar' will comprise of foo and bar variant sub templates for this template file)
+                            SubTemplates =  if ( ! $templateFileConfig['common'] ) {
+                                                $VARIANT['tag'] -split '-' | % { $_.Trim() } | ? { $_ } | ? { $_ -ne $VARIANT['distro'] }
+                                            }else { @() }
+                            Footer = if ( $templateFileConfig['includeFooter'] ) { $true } else { $false }
+                        }
+
+                        $generatedFile = "$( $VARIANT['build_dir'] )/$templateFile"
+                        $templateFileConfig['passes'] | % {
+                            $pass = $_
+                            $templateObject['TemplatePassVariables'] = if ( $pass['variables'] ) { $pass['variables'] } else { @() }
+                            $generatedFile = if ( $pass['generatedFileNameOverride'] ) { "$( $VARIANT['build_dir'] )/$( $pass['generatedFileNameOverride'] )" } else { $generatedFile }
+                            $generatedFileContent = Get-ContextFileContent @templateObject
+                            $generatedFileContent | Out-File $generatedFile -Encoding Utf8 -Force -NoNewline
+                        }
+                    }
+                }
+
+                # Copies
+                if ( $VARIANT['buildContextFiles']['copies'] ) {
+                    $VARIANT['buildContextFiles']['copies'] | % {
+                        $blob = $_.Trim()
+                        # Any blob starting with '/' means we will
+                        if ($blob -match '^\/') {
+                            $fullPathBlob = Join-Path $PROJECT_BASE_DIR $blob
+                        }else {
+                            $fullPathBlob = "$GENERATE_TEMPLATES_DIR/variants/$( $VARIANT['tag'] )/$blob"
+                        }
+                        Copy-Item -Path $fullPathBlob -Destination $VARIANT['build_dir'] -Force -Recurse
+                    }
+                }
+            }
+
+        }
+
+        # Generate other repo files
+        $FILES | % {
+            # Generate README.md
+            Get-ContentFromTemplate -Path (Join-Path $GENERATE_TEMPLATES_DIR "$_.ps1") | Out-File (Join-Path $PROJECT_BASE_DIR $_) -Encoding utf8 -NoNewline
+        }
+    }finally {
+        Pop-Location
+    }
 }
 
 Export-ModuleMember -Function 'Generate-DockerImageVariants'
