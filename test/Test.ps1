a7b4f57e436afbbd2483f1eba5d3f6bfb06866cd
Leo <leonardjonathanoh@live.com>
2019-04-08 09:27:40 +0800

    Update test/Test.ps1

--- test/Test.ps1
+++ test/Test.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $PROJECT_DIR = Split-Path $PSScriptRoot -Parent
+Get-Module Generate-DockerImageVariants | Remove-Module -Force
 Import-Module ( Join-Path ( Join-Path ( Join-Path $PROJECT_DIR 'Modules' ) 'Generate-DockerImageVariants' ) 'Generate-DockerImageVariants.psm1' ) -Force 3>$null
 
 # Generate examples
