6c3e35e7b9616f636d475bcfcda343363edd8969
Leo <leonardjonathanoh@live.com>
2020-03-24 21:50:25 +0000

    Rename ./Modules to ./src

--- test/Test.ps1
+++ test/Test.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $PROJECT_DIR = Split-Path $PSScriptRoot -Parent
 Get-Module Generate-DockerImageVariants | Remove-Module -Force
-Import-Module ( Join-Path ( Join-Path ( Join-Path $PROJECT_DIR 'Modules' ) 'Generate-DockerImageVariants' ) 'Generate-DockerImageVariants.psm1' ) -Force 3>$null
+Import-Module ( Join-Path ( Join-Path ( Join-Path $PROJECT_DIR 'src' ) 'Generate-DockerImageVariants' ) 'Generate-DockerImageVariants.psm1' ) -Force 3>$null
 
 # Generate examples
 Generate-DockerImageVariants -Version
