92405b60e7ac28819560c201c64da7a0e30448fc
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-17 20:48:05 +0000

    Refactor: Improve the sample files generated via -Init

--- src/Generate-DockerImageVariants/samples/generate/templates/Dockerfile.ps1
+++ src/Generate-DockerImageVariants/samples/generate/templates/Dockerfile.ps1
@@ -1,11 +1,24 @@
-@'
+@"
 FROM alpine:3.8
 
+$(if ($VARIANT['components'] -contains 'curl') {
+    @'
 # Install curl
 RUN apk update \
     && apk add --no-cache \
-        curl \
-    && rm -rf /var/cache/apk/*
+        curl
+'@
+})
+
+
+$(if ($VARIANT['components'] -contains 'git') {
+    @'
+# Install git
+RUN apk update \
+    && apk add --no-cache \
+        git
+'@
+})
 
 CMD ["crond", "-f"]
-'@
+"@
