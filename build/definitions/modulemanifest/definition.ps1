370f6642291315284269e656f124cd3f1bb68a16
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-24 05:00:28 +0000

    Enhancement (ci): Add PSModulePublisher submodule
    Closes #30
    
    Module manifest definition aded and module manifest generated.

---
+++ build/definitions/modulemanifest/definition.ps1
@@ -0,0 +1,61 @@
+# - Initial setup: Fill in the GUID value. Generate one by running the command 'New-GUID'. Then fill in all relevant details.
+# - Ensure all relevant details are updated prior to publishing each version of the module.
+# - To simulate generation of the manifest based on this definition, run the included development entrypoint script Invoke-PSModulePublisher.ps1.
+# - To publish the module, tag the associated commit and push the tag.
+@{
+    RootModule = 'Generate-DockerImageVariants.psm1'
+    # ModuleVersion = ''                            # Value will be set for each publication based on the tag ref. Defaults to '0.0.0' in development environments and regular CI builds
+    GUID = 'a6d67f62-ab0a-4c96-bbf1-6c7578c67b04'
+    Author = 'Generate-DockerImageVariants'
+    CompanyName = 'The Oh Brothers'
+    Copyright = '(c) 2019 The Oh Brothers'
+    Description = 'Easily generate a repository populated with Docker image variants.'
+    PowerShellVersion = '3.0'
+    # PowerShellHostName = ''
+    # PowerShellHostVersion = ''
+    # DotNetFrameworkVersion = ''
+    # CLRVersion = ''
+    # ProcessorArchitecture = ''
+    # RequiredModules = @()
+    # RequiredAssemblies = @()
+    # ScriptsToProcess = @()
+    # TypesToProcess = @()
+    # FormatsToProcess = @()
+    # NestedModules = @()
+    FunctionsToExport = @()
+    CmdletsToExport = @(
+        'Generate-DockerImageVariants'
+    )
+    VariablesToExport = @()
+    AliasesToExport = @()
+    # DscResourcesToExport = @()
+    # ModuleList = @()
+    # FileList = @()
+    PrivateData = @{
+        # PSData = @{           # Properties within PSData will be correctly added to the manifest via Update-ModuleManifest without the PSData key. Leave the key commented out.
+            Tags = @(
+                'continuous-deployment'
+                'continuous-integration'
+                'docker'
+                'docker-image'
+                'generator'
+                'module'
+                'powershell'
+                'pwsh'
+                'repository'
+                'template'
+                'template-engine'
+                'variants'
+            )
+            LicenseUri = 'https://raw.githubusercontent.com/theohbrothers/Generate-DockerImageVariants/master/LICENSE'
+            ProjectUri = 'https://github.com/theohbrothers/Generate-DockerImageVariants'
+            # IconUri = ''
+            # ReleaseNotes = ''
+            # Prerelease = ''
+            # RequireLicenseAcceptance = $false
+            # ExternalModuleDependencies = @()
+        # }
+        # HelpInfoURI = ''
+        # DefaultCommandPrefix = ''
+    }
+}
