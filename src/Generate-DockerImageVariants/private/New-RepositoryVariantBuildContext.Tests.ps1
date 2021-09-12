86e62074a58a1352920a9b88bf8c5145c530b6d2
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-09-12 14:39:46 +0000

    Fix: Fix New-RepositoryVariantBuildContext file name not matching function name
    - Renames file from `New-RepositoryVariantContext.ps1` to `New-RepositoryVariantBuildContext.ps1`
    - Renames unit test file from `New-RepositoryVariantContext.Tests.ps1` to `New-RepositoryVariantBuildContext.Tests.ps1`

--- src/Generate-DockerImageVariants/private/New-RepositoryVariantBuildContext.Tests.ps1
+++ src/Generate-DockerImageVariants/private/New-RepositoryVariantBuildContext.Tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "New-RepositoryVariantContext" -Tag 'Unit' {
+Describe "New-RepositoryVariantBuildContext" -Tag 'Unit' {
 
     function Get-ContextFileContent {}
     function New-Item {}
